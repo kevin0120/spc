d9a225cdd1aa3217dec9178137cc59528edd6fae
kevin0120 <jingwei1224@163.com>
2022-04-10 13:20:20 +0800

    go vendor

--- init.go
+++ init.go
@@ -1,5 +1,6 @@
 package spc
 
 import (
-	_ "github.com/kevin0120/spc/v2/wrapper/golang"
+	_ "github.com/kevin0120/spc/v2/include"
+	_ "github.com/kevin0120/spc/v2/wrapper/golang/libs"
 )
--- wrapper/golang/golang.go
+++
@@ -1,6 +0,0 @@
-package golang
-
-import (
-	_ "github.com/kevin0120/spc/v2/wrapper/golang/include"
-	_ "github.com/kevin0120/spc/v2/wrapper/golang/libs"
-)
